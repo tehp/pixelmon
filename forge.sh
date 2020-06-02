432db1efe548795167bbea99b146bc91a609e45b
tehpBOT <>
2020-06-02 06:30:04 +0000

    [auto] Tue Jun  2 06:30:04 UTC 2020

--- forge.sh
+++ forge.sh
@@ -1,7 +1,2 @@
-#!/bin/bash
-start() {
-	sleep 2      
-	cd /opt/pixelmon/forge && java -Xms2G -Xmx2G -jar forge.jar nogui     
-	start 
-} 
-start
+#!/bin/bash      
+cd /opt/pixelmon/forge && java -Xms2G -Xmx2G -jar forge.jar nogui
